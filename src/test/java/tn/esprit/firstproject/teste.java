b9392b11487ed4b183efef29f11ba786595a2c69
rayen-khalfaoui <rayen.khalfaoui@esprit.tn>
2024-07-14 01:48:57 +0200

    added tests now

---
+++ src/test/java/tn/esprit/firstproject/teste.java
@@ -0,0 +1,12 @@
+package tn.esprit.firstproject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class teste {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
